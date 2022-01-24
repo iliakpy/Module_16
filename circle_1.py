76e3672f32b58c6101ed62a1061d084e91f61942
iliakpy <56565312+iliakpy@users.noreply.github.com>
2022-01-24 13:33:24 +0300

    Add files via upload
    Task 16.8.2

---
+++ circle_1.py
@@ -0,0 +1,7 @@
+import math
+class Circle:
+    def __init__(self,a,):
+        self.a = a
+
+    def get_area_circle(self):
+        return math.pi * (self.a ** 2)
---
+++ circle_2.py
@@ -0,0 +1,8 @@
+
+from circle_1 import  Circle
+circle_1 = Circle(5)
+circle_2 = Circle(10)
+
+
+print("Площадь круга", int(circle_1.get_area_circle()))
+print("Площадь круга", int(circle_2.get_area_circle()))
